cccca7975308fd69adad046099a716ba12a69dbc
Iván Fretes <vanfretes@gmail.com>
2020-04-30 19:31:24 -0400

    Mejoras en Schema de Proyecto

--- models/ProyectoModel.js
+++ models/ProyectoModel.js
@@ -11,7 +11,30 @@ const ProyectoSchema = new Schema({
         type : Schema.Types.ObjectId,
         ref : 'User',
     },
+
+    // Necesito 
+    vinculo_requerido : {
+        type : String,
+        enum : ['socio', 'contratar', 'tercierizar', 'no_especificado'],
+        default : 'no_especificado'
+    },
+
+    ciudad : {
+        type : String,
+        default : null
+    },
+    pais : {
+        type : String,
+        default : null
+    },
+
+    posibilidad_remoto : { 
+        type : 'String',
+        enum : ['no', 'si', 'no_especificado'],
+        default : 'no_especificado'
+    },
     
+    // Listado de necesidades para el proyecto
     recursos_necesarios : [
         {
             type : String, 
